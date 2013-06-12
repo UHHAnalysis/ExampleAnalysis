d09b2fe0e212fa63ed827eadec372c0641b895f8
Thomas Peiffer <peiffer@tcx101.naf.desy.de>
2013-06-12 16:58:06 +0200

    missing proof libs

--- proof/SETUP.C
+++ proof/SETUP.C
@@ -4,6 +4,14 @@ int SETUP() {
 
    /// Add all the additional libraries here that this package
    /// depends on. (With the same command that loads this package's library...)
+   if( gSystem->Load( "libfastjet" ) == -1 ) return -1;
+   if( gSystem->Load( "libfastjettools" ) == -1 ) return -1;
+   if( gSystem->Load( "libJetMETObjects" ) == -1 ) return -1;
+   if( gSystem->Load( "libTree" ) == -1 ) return -1;
+   if( gSystem->Load( "libHist" ) == -1 ) return -1;
+   if( gSystem->Load( "libGraf" ) == -1 ) return -1;
+   if( gSystem->Load( "libSFrameTools" ) == -1 ) return -1;
+   if( gSystem->Load( "libAnalysis" ) == -1 ) return -1;
    if( gSystem->Load( "libExampleAnalysis" ) == -1 ) return -1;
 
    return 0;
